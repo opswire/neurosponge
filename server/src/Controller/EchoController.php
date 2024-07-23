a364b12d076f23de01c5e1e06b22ce079e7805e5
stanislav <stchikhichin@gmail.com>
2024-07-23 22:41:32 +0300

    swagger added

--- server/src/Controller/EchoController.php
+++ server/src/Controller/EchoController.php
@@ -3,12 +3,37 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class EchoController
 {
-    #[Route('/echo', name: 'echo')]
+    /**
+     * Узнать статус приложения
+     */
+    #[OA\Response(
+        response: 200,
+        description: 'Возвращает статус ответа',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    property: "data",
+                    type: "string",
+                    example: "success"
+                ),
+                new OA\Property(
+                    property: "status",
+                    type: "integer",
+                    example: "200"
+                )
+            ],
+            type: 'object',
+        )
+    )]
+    #[OA\Tag(name: 'Test')]
+    #[Route('/api/echo', name: 'echo', methods: ['GET'])]
     public function echo(): JsonResponse
     {
         return new JsonResponse([
