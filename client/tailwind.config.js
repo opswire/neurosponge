bce7575abfc87360df14dc665f03a20cda454a85
farsmellah <the.real.smartfellah@gmail.com>
2024-07-23 16:41:27 +0300

    style: add Inter font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
