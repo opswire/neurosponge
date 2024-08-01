eadd221378ac92315a2a23433508d184be0fe3f4
farsmellah <the.real.smartfellah@gmail.com>
2024-08-01 20:26:29 +0300

    feat(wip): add search functionality

--- client/src/app/search/page.tsx
+++ client/src/app/search/page.tsx
@@ -1,10 +1,19 @@
-"use client";
+"use server";
 
+import { DeckList, getAllDecks, GetAllDecksQueryParams } from "@/entities";
 import { AutoComplete } from "./components/autocomplete-bar";
 import { SortMenu } from "./components/sort-menu";
+import { SeparatorHorizontal } from "lucide-react";
 
-export default function SearchPage() {
-  const submitSearch = (value: string) => {};
+export default async function SearchPage({
+  searchParams,
+}: {
+  searchParams: GetAllDecksQueryParams;
+}) {
+  const {} = searchParams;
+  const data = await getAllDecks({
+    ...searchParams,
+  });
   return (
     <main className="xl:max-w-screen-xl lg:max-w-screen-lg mx-auto flex flex-col items-stretch mt-12 px-8 gap-12">
       <div className="hidden gap-10 w-full flex-auto sm:flex justify-between items-end">
@@ -25,8 +34,16 @@ export default function SearchPage() {
         <SortMenu className="flex-grow w-full" />
       </div>
 
-      <div>
-        <h1>Результаты поиска по </h1>
+      <div className="flex flex-col gap-8">
+        <h1>
+          Результаты поиска по{" "}
+          <span className="text-lg font-semibold">
+            "{searchParams["filter[search]"]}"
+          </span>
+        </h1>
+        <div>
+          <DeckList decks={data.data} />
+        </div>
       </div>
     </main>
   );
